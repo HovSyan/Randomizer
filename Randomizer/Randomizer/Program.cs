884dba181f5b1cfafc9145716cb32f9029a74cb7
Hovhannes <syanhovhannes@gmail.com>
2018-04-14 09:31:29 +0400

    [add] randomizer.sln

---
+++ Randomizer/Randomizer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string[] objects;
+            int index = 0;
+            Random random = new Random();
+
+            Console.WriteLine(StringOutputs.GREETING_MESSEGE);
+            Console.WriteLine(StringOutputs.STYLE);
+            Console.WriteLine(StringOutputs.OPERATING_GUIDE);
+            Console.WriteLine(StringOutputs.STYLE);
+
+            Console.Write("Number of objects: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            objects = new string[n];
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write(i.ToString() + ". ");
+                objects[i - 1] = Console.ReadLine();
+            }
+
+            Console.WriteLine(StringOutputs.STYLE);
+
+            index = random.Next(1, n) - 1;
+
+            Console.WriteLine((index + 1).ToString() + ". " + objects[index]);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
---
+++ Randomizer/Randomizer/StringOutputs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomise
+{
+    abstract class StringOutputs
+    {
+        public static string GREETING_MESSEGE = "Hi, you are welcomed by programm \"Randomise\"." + "\n" +
+                                                "This program chooses a single thing randomly from" + "\n" +
+                                                "many gived ones.";
+        public static string OPERATING_GUIDE = "Please, type number of objects, from which the" + "\n" +
+                                               "program will choose. Then type the objects.";
+        public static string STYLE = "----------------------------------------------------"; 
+    }
+}
